4a1eb1ec1bfc80c2cb68cba7375ae0e63a8fed2f
maajek <majekodunmi>
2018-03-12 14:20:11 +0100

    My first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// Index.android.js - place code in here for android
+
+import React from 'react';
+import { AppRegistry, View }from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
+
+const App = () => (
+	<View style={{ flex: 1 }}>
+		<Header headerText={'Albums'}/>
+		<AlbumList />
+	</View>
+		);
+
+AppRegistry.registerComponent('pics', () => App);
+
+  
+   
